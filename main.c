c2278bf7d8cf32e4ff7887810868e1a8fdd8e29f
basemaboudeshish <101295055+basemaboudeshish@users.noreply.github.com>
2022-10-12 23:45:43 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "../Application/app.h"
+#include "../Card/card.h"
+#include "../Server/server.h"
+#include "../Application/app.h"
+#include "main.h"
+
+main(){
+appstart();
+printf(" ---Exit the programm\n");
+return 0;
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "../Application/app.h"
+#include "../Card/card.h"
+#include "../Server/server.h"
+#include "../Application/app.h"
+
+
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+void main(void);
+
+
+#endif
